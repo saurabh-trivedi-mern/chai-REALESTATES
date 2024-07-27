5c29caf1b7405fb9c0b8c92715f0379952c2686e
Saurabh Trivedi <saurabhtrivedi2024@gmail.com>
2024-07-28 00:51:59 +0530

    Initial Files

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find A Home For You, Not Just A Property.</h1>
+          <p>
+          Your journey to homeownership starts here. The answer to your real estate needs ends here. 
+          Buying a home: the best decision you'll make since sliced bread.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>7+</h1>
+              <h2>Years of Happy Homes</h2>
+            </div>
+            <div className="box">
+              <h1>1500+</h1>
+              <h2>Houses Sold</h2>
+            </div>
+            <div className="box">
+              <h1>3500+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default HomePage;
